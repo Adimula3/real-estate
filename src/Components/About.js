fd66b6be4a46b5f36716e347db263307348e7f04
Jos33y <josephlagbalu@gmail.com>
2022-02-13 16:25:28 +0100

    worked on col-xl

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,18 +7,18 @@ function About(props) {
                 <div className="container">
 
                     <div className="row">
-                        <div className="col-xl-6 col-lg-7">
+                        <div className="col-xl-5 col-lg-5">
                             <img src={require('../Img/michael-oxendine-GHCVUtBECuY-unsplash.jpg')} alt="" />
                         </div>
 
-                        <div className="col-xl-6 col-lg-6 d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5">
+                        <div className="col-xl-7 col-lg-7 d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5">
                             <div className="box-heading">
                                 <h4>About us</h4>
                                 <h3>A REAL-ESTATE COMPANY</h3>
                                 <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean</p>
                             </div>
                                  <div className="counts">
-                                     <div className="row counters ">
+                                     <div className="row counters">
                                          <div className="col-lg-2 col-md-4 col-6">
                                              <span className="count">5</span>
                                              <p>Years of experience</p>
