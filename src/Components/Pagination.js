250fd712149886992777acc81b2b6143974b6842
Adimula3 <Kolawoleolayinka16@gmail.com>
2022-03-31 03:43:52 -0500

    Testing 1

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -1,64 +1,70 @@
 import React, {useEffect, useState} from 'react';
 import ReactPaginate from "react-paginate";
+import HousingModal from "./HousingModal";
+import PropertyDetails from "./Property-details";
 
 
 const PER_PAGE = 8;
 function Pagination() {
     const [currentPage, setCurrentPage] = useState(0);
     const [data, setData] = useState([]);
+    const [isQuickView, setQuickView] = useState(false);
 
 
     useEffect(() => {
         fetchData();
     }, []);
 
-
-
     function fetchData() {
         fetch('http://localhost:8000/houses')
             .then((res) => res.json())
             .then ((data) => {
                 setData(data);
-                console.log(data);
+                // console.log(data);
             });
     }
 
     function  handlePageClick({selected: selectedPage}) {
-        console.log("selectedPage", selectedPage);
+        //console.log("selectedPage", selectedPage);
         setCurrentPage(selectedPage);
+
+
     }
 
-    // 0, 10, 20, 30......//
+    // 0, 8, 16./...../
     const offset = currentPage * PER_PAGE;
-    // console.log("offset", offset)
+     console.log("offset", offset)
 
     const currentPageData = data
         .slice(offset, offset + PER_PAGE)
-        .map((res, index) => <img key={index} src={res.url} /> );
-    //console.log("currentPage", currentPage);
+        .map((res, index) =>
+            <div className="row">
+            <div className="col-lg-3 col-6">
+            <div className="card" onClick={() => {setQuickView(true)}}>
+                <img key={index} src={res.url} />
+                <div className="card-body">
+                    <div className="list-team d-flex">
+                        <h6 className="ml-2">Joseph doe</h6>
+                        <span className="text-right">2 weeks ago</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+                {/** Modals Here */}
+                <HousingModal open={isQuickView} onClose={() => setQuickView(false)}>
+                    <PropertyDetails />
+                </HousingModal>
+            </div> );
+
+      console.log("currentPage", currentPageData);
 
     // total pages
     const pageCount = Math.ceil(data.length / PER_PAGE);
     return (
+
         <div>
+            {currentPageData}
 
-            {data.map((data) => {
-                return <div className="col-lg-3 col-6">
-                    <div className="card">
-                        <img src={data.url} className="card-img-top" alt=""/>
-                        <div className="card-body">
-                            <div className="list-team d-flex">
-                                <h6 className="ml-2">Joseph doe</h6>
-                                <span className="text-right">2 weeks ago</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>;
-
-                }
-
-            )}
-            
             <ReactPaginate
             previousLabel={"<- Previous"}
                 nextLabel={"Next ->"}
