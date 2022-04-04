206f8060e80828bd04c4dec2c569a47bd18eba16
Adimula3 <Kolawoleolayinka16@gmail.com>
2022-04-04 16:46:35 -0500

    Testing 1

--- src/Components/Crud.js
+++ src/Components/Crud.js
@@ -2,52 +2,57 @@ import React from 'react';
 
 function Crud() {
     return (
+            <div className="crud">
 
-            <form>
-                <div className="form-row">
-                    <div className="name">Agent-Name</div>
-                    <input className="input-group" type="text"/>
-                </div>
-                <div className="form-row">
-                    <div className="name">Email</div>
-                    <input className="input-group" type="email"/>
-                </div>
-                <div className="form-row">
-                    <div className="name">Phone-number</div>
-                    <input className="input-group" type="text"/>
-                </div>
-                <div className="form-row">
-                    <div className="property-name">Property-Name:</div>
-                    <input className="input-group" type="text"/>
-                </div>
-                <div className="form-row">
+                <div className="container">
+                    <form className="row g3">
+                        <div className="col-md-6 form-group">
+                            <div className="name">Agent-Name</div>
+                            <input type="name" className="form-control"/>
+                        </div>
+                        <div className="col-md-6 form-group">
+                            <div className="name">Email</div>
+                            <input type="email" className="form-control"/>
+                        </div>
+                        <div className="col-md-4 form-group">
+                            <div className="name">Phone-number</div>
+                            <input type="text" className="form-control"/>
+                        </div>
+                        <div className="col-md-4 form-group">
+                            <div className="name">Property-name</div>
+                            <input type="name" className="form-control"/>
+                        </div>
+                        <div className="col-md-4 form-group">
+                            <div className="name">Status</div>
+                            <select className="form-select">
+                                <option selected>choose..</option>
+                                <option>Rent</option>
+                                <option>Sale</option>
+                            </select>
+                        </div>
+                        <div className="col-12 form-group">
+                            <div className="name">Location</div>
+                            <input type="text" className="form-control"/>
+                        </div>
+                        <div className="col-12 form-group">
+                            <div className="name">Property-details</div>
+                            <textarea className="form-control" placeholder="property-details"></textarea>
+                        </div>
+                        <div className="col-md-2 form-group">
+                            <div className="Upload image">
+                                <input className="form-control" type="file"/>
+                            </div>
+                        </div>
+                        <div className="col-12 form-group">
+                            <button type="submit" className="btn btn-primary">submit</button>
+                        </div>
+                    </form>
 
-                    <div className="name">Location:</div>
-                    <input className="input-group" type="text"/>
                 </div>
-                <div className="form-row">
-
-                    <div className="name">Rent</div>
-                    <input type="checkbox"/>
-                    <div className="name">Sale</div>
-                    <input type="checkbox"/>
-                </div>
-                <div className="form-row">
-                    <div className="name">Property-details</div>
-                    <textarea className="input-group" placeholder="Property-details"></textarea>
-
-                </div>
-                <div className="form-row">
-                    <div className="name">Upload-Image</div>
-                    <input className="input-group" type="file"/>
-                </div>
-
-                <div className="button">
-                    <button className="btn" type="submit">Upload-property</button>
-                </div>
+            </div>
 
 
-            </form>
+
 
 
     );
