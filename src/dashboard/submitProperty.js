e7ba46b147268d759a072d456018bc7a2a4615e3
Adimula3 <Kolawoleolayinka16@gmail.com>
2022-04-17 17:35:25 -0500

    Testing 1

--- src/dashboard/submitProperty.js
+++ src/dashboard/submitProperty.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Col, Row} from "react-bootstrap";
+import {Button, Col, Form, FormSelect, Row} from "react-bootstrap";
 import TopBar from "./components/topbar";
 import Sidebar from "./components/sidebar";
 import "./styles/dashboard.css";
@@ -27,6 +27,120 @@ const SubmitProperty = () => {
                                         <Row>
                                             <div className="dashboard-panel w-100">
                                                 <h4 className="text-secondary">Submit Property</h4>
+                                                <div className="submitProperty form-border p-5 bg-white">
+                                                    <Form className="form-box">
+                                                        <div className="description">
+                                                            <h5 className="text-secondary">Basic Information</h5>
+                                                            <hr/>
+                                                            <div className="form-group">
+                                                                <Row>
+                                                                    <Col md={6}>
+                                                                        <label className="name">Agent-Name</label>
+                                                                        <input type="name" className="form-control"/>
+                                                                    </Col>
+                                                                    <Col md={6}>
+                                                                        <label className="name">Property-title</label>
+                                                                        <input type="text" className="form-control"/>
+                                                                    </Col>
+                                                                </Row>
+                                                            </div>
+                                                            <div className="form-group">
+                                                                <Row>
+                                                                    <Col md={6}>
+                                                                        <FormSelect className="select">
+                                                                            <option>Any status</option>
+                                                                            <option value="1">Sale</option>
+                                                                            <option value="2">Rent</option>
+                                                                        </FormSelect>
+                                                                    </Col>
+                                                                    <Col md={6}>
+                                                                        <FormSelect className="select">
+                                                                            <option>Any status</option>
+                                                                            <option value="1">Sale</option>
+                                                                            <option value="2">Rent</option>
+                                                                        </FormSelect>
+                                                                    </Col>
+                                                                </Row>
+                                                            </div>
+                                                            <div className="form-group">
+                                                                <Row>
+                                                                    <Col md={4}>
+                                                                        <label className="name">Price</label>
+                                                                        <input type="text" className="form-control"/>
+                                                                    </Col>
+                                                                    <Col md={4}>
+                                                                        <label className="name">Area</label>
+                                                                        <input type="text" className="form-control"/>
+                                                                    </Col>
+                                                                    <Col md={4}>
+                                                                        <label className="name">Bed Rooms</label>
+                                                                        <input type="number" min="1" max="9" step="1" value="1" className="form-control"/>
+                                                                    </Col>
+                                                                </Row>
+                                                            </div>
+                                                        </div>
+                                                        <div className="description">
+                                                            <h5 className="text-secondary">Description</h5>
+                                                            <hr></hr>
+                                                            <div className="form-group">
+                                                                <Row>
+                                                                    <Col lg={12}>
+                                                                        <label className="name">Property-details</label>
+                                                                        <textarea name="description" id="description" cols="30" rows="5" className="form-control" placeholder="write details...."></textarea>
+                                                                    </Col>
+                                                                </Row>
+                                                            </div>
+                                                        </div>
+                                                        <div className="property-location">
+                                                            <h5 className="text-secondary">Property Location</h5>
+                                                            <hr></hr>
+                                                            <div className="form-group">
+                                                                <Row>
+                                                                    <Col md={12}>
+                                                                        <label className="name">Address</label>
+                                                                        <input type="text" className="form-control"/>
+                                                                    </Col>
+                                                                </Row>
+                                                            </div>
+                                                            <div className="form-group">
+                                                                <Row>
+                                                                    <Col md={4}>
+                                                                        <label className="name">State</label>
+                                                                        <input type="text" className="form-control"/>
+                                                                    </Col>
+                                                                    <Col md={4}>
+                                                                        <label className="name">Zip Code</label>
+                                                                        <input type="text" className="form-control"/>
+                                                                    </Col>
+                                                                    <Col md={4}>
+                                                                        <label className="name">City</label>
+                                                                        <input type="text"  className="form-control"/>
+                                                                    </Col>
+                                                                </Row>
+                                                            </div>
+                                                        </div>
+                                                        <div className="upload-media mt-5">
+                                                            <h5 className="text-secondary">Add Photos and Videos</h5>
+                                                            <hr></hr>
+                                                            <div className="form-group">
+                                                                <Row>
+                                                                    <Col md={12}>
+                                                                        <input type="file" id="fileupload-example-1" className="d-none"/>
+                                                                            <label className="fileupload-label text-center w-100" htmlFor="fileupload-example-1">Drag and Drop to Add Photo (770x390)</label>
+                                                                    </Col>
+                                                                </Row>
+                                                            </div>
+                                                        </div>
+                                                        <div className="form-group">
+                                                            <Row>
+                                                                <Col md={4}>
+                                                                    <Button type="submit" className="btn btn-primary">Submit</Button>
+                                                                </Col>
+                                                            </Row>
+                                                        </div>
+                                                    </Form>
+
+                                                </div>
                                             </div>
                                         </Row>
                                     </section>
